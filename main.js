ffb9aeeb7f2a60901cf8aa22c05aeb0f23be98cf
soumen <soumensen411@gmail.com>
2024-08-03 22:47:04 +0600

    insta-effect

--- main.js
+++ main.js
@@ -1,17 +1,12 @@
-var istatus = document.querySelector("h1")
-var addFriend = document.querySelector(".add")
-var remove = document.querySelector(".Remove")
-var flag = 0;
-
-addFriend.addEventListener("click",function(){
-    istatus.innerHTML="Friend"
-    istatus.style.color = "green"
-    
-    
-})
-remove.addEventListener("click",function(){
-
-    istatus.innerHTML="Stranger"
-    istatus.style.color = "red"
+var container = document.querySelector(".container")
+var love = document.querySelector("i")
+container.addEventListener("dblclick",function(){
+    love.style.transform = "translate(-50%,-50%) scale(1)"
+    setTimeout(function(){
+        love.style.transform = "translate(-50%,-50%) scale(0)"
+    },2000);
+    setTimeout(function(){
+        love.style.transform = "translate(-50%,-50%) scale(0)"
+    },2000);
     
 })
